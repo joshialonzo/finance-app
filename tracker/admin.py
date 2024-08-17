852e7c4b2c1140df32d83eb5d09b8117792cebaf
Josue Alonzo-Chavarria <josue.alonzo@motimatic.com>
2024-08-16 23:19:09 -0600

    Register in admin: Category and Transaction

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from tracker.models.categories import Category
+from tracker.models.transactions import Transaction
+
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Transaction)
